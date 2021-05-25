49839db9521b836e1b6f5117cd16be2cf1c67ac2
Shartzzz-work <workshartzzz@gmail.com>
2021-05-25 21:39:12 +0300

    deploy

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let bool = false;
+
 class NavEvent {
   handleEvent(event) {
 
@@ -7,25 +9,41 @@ class NavEvent {
           document.querySelector(".hamburger").classList.toggle("active");
           document.querySelector(".nav__menu").classList.toggle("active");
         }
+
         if (event.target.classList.contains('settings_icon')) {
           document.querySelector(".settings").classList.toggle("active");
         }
-        break;
-      case 'mouseover':
-        if (event.target.classList.contains('nav-link')) {
-          console.log(event);
-          document.querySelector(".nav__menu").insertAdjacentHTML('afterbegin', `
-            <span class="nav__menu-span"></span>
-          `);
+
+        if (event.target.classList.contains('message__settings')) {
+          bool = !bool;
+          console.log(bool);
+          if (bool == true) {
+            document.querySelector(".message__body").insertAdjacentHTML('afterbegin',
+              `
+              <div class="message__settings-active">
+                <div class="message__settings_item">
+                  <img src="../assets/img/prstar1.jpg">
+                  <h3 class="glass">Prstar</h3>
+                </div>
+                <div class="message__settings_item active">
+                  <img src="../assets/img/prstar2.jpg">
+                  <h3 class="glass">Active</h3>
+                </div>
+                <div class="message__settings_item">
+                  <img src="../assets/img/prstar4.jpg">
+                  <h3 class="glass">Prstar</h3>
+                </div>
+              </div>
+            `
+            );
+          } else {
+            document.querySelector(".message__settings-active").remove();
+          }
         }
-        break;
-      case 'mouseout':
         break;
     }
   }
 }
 
 let navEvent = new NavEvent;
-header.addEventListener('click', navEvent);
-header.addEventListener('mouseover', navEvent);
-header.addEventListener('mouseout', navEvent);
+document.addEventListener('click', navEvent);
