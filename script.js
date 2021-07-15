0f7fdbfe6aa018e6449cd41db83dcc81e44c6390
Shartzzz-work <workshartzzz@gmail.com>
2021-07-15 18:22:59 +0300

    willPost

--- script.js
+++ script.js
@@ -93,10 +93,8 @@ document.querySelectorAll('.open-modal').forEach(i => i.addEventListener('click'
 
   document.querySelectorAll('.modal__wrapper').forEach(i => i.addEventListener('click', () => document.getElementById(modalId).style.display = ""));
 
-  console.log(i.childNodes[2].dataset.city);
-
-  document.querySelector('.changeCity').value = `${i.childNodes[2].dataset.city}`;
-  document.querySelector('.changeStreet').value = `${i.childNodes[3].dataset.street}`;
+  document.querySelector('.changeCity').value = `${i.childNodes[2].innerText}`;
+  document.querySelector('.changeStreet').value = `${i.childNodes[4].innerText}`;
 })
 )
 
