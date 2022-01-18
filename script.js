994c6a380a3f99c72d48da9c0e54e95487e206bc
Shartzzz-work <workshartzzz@gmail.com>
2022-01-18 05:32:36 +0300

    сообщения

--- script.js
+++ script.js
@@ -21,42 +21,43 @@ class NavEvent {
         //   modal.style = "";
         // }
 
-        if (event.target.classList.contains('message__settings_img')) {
+        if (event.target.closest('.message__settings')) {
           bool = !bool;
-          console.log(bool);
+          // console.log(bool);
           if (bool == true) {
-            document.querySelector(".message__body").insertAdjacentHTML('afterbegin',
-              `
-              <div class="message__settings-active">
-                <div class="message__settings_item">
-                  <img src="../assets/img/prstar1.jpg">
-                  <h3 class="glass">Prstar</h3>
-                </div>
-                <div class="message__settings_item active">
-                  <img src="../assets/img/prstar2.jpg">
-                  <h3 class="glass">Active</h3>
-                </div>
-                <div class="message__settings_item">
-                  <img src="../assets/img/prstar4.jpg">
-                  <h3 class="glass">Prstar</h3>
-                </div>
-                <div class="message__settings_item">
-                  <img src="../assets/img/prstar4.jpg">
-                  <h3 class="glass">Prstar</h3>
-                </div>
-                <div class="message__settings_item">
-                  <img src="../assets/img/prstar4.jpg">
-                  <h3 class="glass">Prstar</h3>
-                </div>
-                <div class="message__settings_item">
-                  <img src="../assets/img/prstar4.jpg">
-                  <h3 class="glass">Prstar</h3>
-                </div>
-              </div>
-            `
-            );
+            document.querySelector(".message__settings-active").style.display = "flex"
+            // document.querySelector(".message__body").insertAdjacentHTML('afterbegin',
+            //   `
+            //   <div class="message__settings-active">
+            //     <div class="message__settings_item">
+            //       <img src="../assets/img/prstar1.jpg">
+            //       <h3 class="glass">Prstar</h3>
+            //     </div>
+            //     <div class="message__settings_item active">
+            //       <img src="../assets/img/prstar2.jpg">
+            //       <h3 class="glass">Active</h3>
+            //     </div>
+            //     <div class="message__settings_item">
+            //       <img src="../assets/img/prstar4.jpg">
+            //       <h3 class="glass">Prstar</h3>
+            //     </div>
+            //     <div class="message__settings_item">
+            //       <img src="../assets/img/prstar4.jpg">
+            //       <h3 class="glass">Prstar</h3>
+            //     </div>
+            //     <div class="message__settings_item">
+            //       <img src="../assets/img/prstar4.jpg">
+            //       <h3 class="glass">Prstar</h3>
+            //     </div>
+            //     <div class="message__settings_item">
+            //       <img src="../assets/img/prstar4.jpg">
+            //       <h3 class="glass">Prstar</h3>
+            //     </div>
+            //   </div>
+            // `
+            // );
           } else {
-            document.querySelector(".message__settings-active").remove();
+            document.querySelector(".message__settings-active").style.display = "none";
           }
         }
 
